d0e22287dd3878877a80e55fe93bbe4ac4a471bc
chongsuk21 <chongsuk21@gmail.com>
2022-11-17 22:55:24 -0500

    added some comments

--- main.py
+++ main.py
@@ -18,18 +18,13 @@ with open("sample-input.txt", "r") as f:
 
 lines=[]
 for x in temp_lines:
-    if not (x.startswith('#') or x.startswith('\n')):
+    if not (x.startswith('#') or x.startswith('\n')): #if the line starts with # or '\n' just ignore
        lines.append(x)
 
 
-for line in lines:
+for line in lines:#the outermost loop
     print(line)
     print()#line break
-
-    n=6
-
-    demo = list([line[i:i+n] for i in range(0, len(line), n)])
-
 
     w, h = 6, 6
     arr = [[0 for x in range(w)] for y in range(h)]
@@ -57,41 +52,41 @@ for line in lines:
     for i in range(6):
         print(arr[i])
 
-    visited=list()
-    # a car (length, row, column, is_horizontal, name, list of possible moves, fuel)
-    cars = []
+    visited=list() #visited list, it checkes if the car is already visited
 
-    for i in range(6):
+    cars = []# a car (length, row, column, is_horizontal, name, list of possible moves, fuel)
+
+
+    for i in range(6): #6x6
         for j in range(6):
             if arr[i][j] not in visited and arr[i][j] != '.': #if the current elm is not visited AND is not '.'
-                i_row = i
+                i_row = i #initial row
                 i_col = j #initial col
-                row = i_row #saving initial position
-                col = i_col
                 if j != 5 and arr[i][j+1] == arr[i][j]: #if arr[i][j] is horizontal   , when j ==5 then it's the last elm of the line
-                    n = 1
-                    r_move = 0
-                    l_move = 0
+                    n = 1 # n is size
+                    r_move = 0 #right move
+                    l_move = 0 #left move
                     h_moves = []
                     temp_col = j
-                    if arr[i][j] in fuel_dict:
+                    if arr[i][j] in fuel_dict: #if fuel is specified
                         fuel = int(fuel_dict[arr[i][j]])
-                    else:
+                    else: #otherwise fule is 100
                         fuel = 100
-                    visited.append(arr[i][j])
-                    while j+n != 6 and arr[i][j+n] == arr[i][j]: # while will run untill the last position of the car
-                        n += 1
-                    while temp_col+n != 6 and arr[i][temp_col+n] == '.': #arr[i][j+n] is the last position
+                    visited.append(arr[i][j]) #arr[i][j] which is the current elm is visited now
+                    while j+n != 6 and arr[i][j+n] == arr[i][j]: # while loop will run untill the last position of the car
+                        n += 1 #n is size
+                    while temp_col+n != 6 and arr[i][temp_col+n] == '.': #temp_col is just 'j' which is initial col number + n (car size) so arr[i][temp_col+n] is the next elm of the last position of car (in other words, it's checking if there is an empty space on the right side
                       r_move +=1
                       temp_col += 1
                       f_elm = "R" + str(r_move)
                       h_moves.append(f_elm)
-                    while col-1 >= 0 and arr[i][col-1] == '.': #if the first position -1 is '.'
+                    temp_col = i_col #temp_col is back to initial col
+                    while temp_col-1 >= 0 and arr[i][temp_col-1] == '.': #Back to the first position of the car to check if there is an empty space on the left side
                       l_move +=1
-                      col -= 1
+                      temp_col -= 1
                       b_elm = "L" + str(l_move)
                       h_moves.append(b_elm)
-                    cars.append([n, i_row, i_col, True, arr[i][j], h_moves, fuel])
+                    cars.append([n, i_row, i_col, True, arr[i][j], h_moves, fuel]) #car appended
                 elif i != 5: #if arr[i][j] is vertical
                     m = 1
                     u_move = 0
@@ -104,15 +99,16 @@ for line in lines:
                         fuel = 100
                     visited.append(arr[i][j])
                     while i+m != 6 and arr[i+m][j] == arr[i][j]:
-                        m += 1
-                    while temp_row+m != 6 and arr[temp_row+m][j] == '.' : #arr[i+m][j] is the last position
+                        m += 1 # m is size
+                    while temp_row+m != 6 and arr[temp_row+m][j] == '.' :
                       d_move +=1
                       temp_row += 1
                       f_elm = "D" + str(d_move)
                       v_moves.append(f_elm)
-                    while row-1 >= 0 and arr[row-1][j] == '.': #if the first position -1 is '.'
+                    temp_row = i_row #temp_row is back to initial row
+                    while temp_row-1 >= 0 and arr[temp_row-1][j] == '.': #if the first position -1 is '.'
                       u_move +=1
-                      row -= 1
+                      temp_row -= 1
                       b_elm = "U" + str(u_move)
                       v_moves.append(b_elm)
                     cars.append([m, i_row, i_col, False, arr[i][j], v_moves, fuel])
